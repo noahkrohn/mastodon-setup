e464e3c171ec76b8b3a0e78849eaae9256394433
Noah Krohn <10424053+noahkrohn@users.noreply.github.com>
2024-03-18 20:41:28 -0400

    update backend and network

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,7 @@ terraform {
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    force_path_style            = true
+    use_path_style              = true
+    skip_requesting_account_id  = true
   }
 }
